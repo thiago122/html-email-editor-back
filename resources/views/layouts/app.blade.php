fc95e5751775d82f4519e46d4d15ef0ef982e338
Thiago <thiago122@gmail.com>
2025-03-17 13:24:40 -0300

    ordem dos scripts do livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,12 +21,13 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @wireUiScripts
+        
         @livewireScripts
-        @livewireStyles
+        @wireUiScripts
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
+        @livewireStyles
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             <livewire:layout.navigation />
             <x-notifications />
