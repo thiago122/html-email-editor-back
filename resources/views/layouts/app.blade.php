7f8488c33db448c24d1bed2a5d7381be13a481dd
Thiago <thiago122@gmail.com>
2025-05-13 11:51:21 -0300

    ajuste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        
+        @livewireStyles
         @livewireScripts
         @wireUiScripts
         @vite(['resources/css/app.css', 'resources/js/app.js'])
